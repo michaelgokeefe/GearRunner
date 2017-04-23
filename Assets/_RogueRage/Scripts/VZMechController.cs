4c4a7733ddba34a62a5ed13112ece880f07a363c
Jesse Comb <jesse@nomadic.digital>
2017-04-22 18:27:53 -0700

    drone update

--- Assets/_RogueRage/Scripts/VZMechController.cs
+++ Assets/_RogueRage/Scripts/VZMechController.cs
@@ -9,9 +9,6 @@ public class VZMechController : MonoBehaviour {
     public Transform PlayerRoot;
     public Transform MechTorso;
     public Camera HeadCamera;
-    public Transform CameraTransform;
-    public Transform CameraRigPC;
-
     public AnimationCurve StrafeThresholds;
     public float TurningAngleThreshold;
     public float HeadBikeFactor;
@@ -37,7 +34,7 @@ public class VZMechController : MonoBehaviour {
     //public float bikeZAngle;
     // Use this for initialization
     public static VZController VZController { get; private set; }
-    public bool IsCalibrated;
+
     public float NeckHeight;
     public float NeckForwardOffset;
 
@@ -61,7 +58,7 @@ public class VZMechController : MonoBehaviour {
 
     public float MinDistanceToLean;
     public float MaxLeanDelta;
-    void Awake () {
+    void Awake() {
 
 
 
@@ -81,15 +78,15 @@ public class VZMechController : MonoBehaviour {
         }
 
         // Attach controller to us
-        VZController.AttachPlayer(MechTorso, HeadCamera);
+        //VZController.AttachPlayer(MechTorso, HeadCamera);
 
 
     }
     void Start() {
-        _MotionInput.OnMenu = false;
+        //_MotionInput.OnMenu = false;
 
         // Transition canvas
-        VZController.TransitionCanvas().GetComponent<Canvas>().enabled = true;
+        //VZController.TransitionCanvas().GetComponent<Canvas>().enabled = true;
 
         //// Offset raycast for terrain
         //Transform raycastOrigin = transform.Find("RaycastOrigin");
@@ -102,79 +99,16 @@ public class VZMechController : MonoBehaviour {
 
 
         // Lookup release screen
-        Transform release = VZController.TransitionCanvas().transform.Find("Release");
+        //Transform release = VZController.TransitionCanvas().transform.Find("Release");
 
-        if (release != null) {
-            _TransitionRelease = release.GetComponent<Text>();
-        }
+        //if (release != null) {
+        //    _TransitionRelease = release.GetComponent<Text>();
+        //}
 
 
-        IsCalibrated = false;
+        //IsCalibrated = false;
     }
 
-    void UpdateCalibrating() {
-        // Try connecting to bike
-        if (VZController.BikeState().Type < 0)
-            VZController.TryConnectBike();
-
-        // Update calibration msg
-        if (_TransitionRelease != null) {
-            _TransitionRelease.text = RELEASE_TEXT + "\n\n\n";
-
-#if !UNITY_EDITOR
-         // Require bike outside of editor
-         if (VZController.BikeState().Type < 0)
-            _TransitionRelease.text += "PLUG IN DONGLE";
-         else if (!VZController.BikeState().Connected)
-            _TransitionRelease.text += "POWER ON BIKE";
-         else
-#endif
-            _TransitionRelease.text += "HOLD L+R TO BEGIN";
-        }
-
-        // Hold both buttons to calibrate
-        if (VZController.LeftButton.Held(0.5f) && VZController.RightButton.Held(0.5f) && VZController.IsHeadTracked()) {
-
-
-            VZController.LeftButton.Clear();
-            VZController.RightButton.Clear();
-
-            NeckHeight = PlayerInput.transform.localPosition.y - 0.8f;
-            // Init neck height
-            VZController.Neck().localPosition = new Vector3(0, NeckHeight, NeckForwardOffset);
-
-            VZController.Recenter();
-
-            Restart(true);
-            Debug.Log(PlayerInput.transform.parent.name + " : " + PlayerInput.transform.parent.localEulerAngles.y);
-            PlayerInput.YawOffset = PlayerInput.transform.parent.localEulerAngles.y - 360;
-            PlayerInput.YawTransform = PlayerInput.transform;
-        }
-    }
-    protected virtual void Restart(bool initLevel) {
-
-        // Reset transition canvas alpha
-        VZController.TransitionCanvas().GetComponent<CanvasGroup>().alpha = 1;
-
-        // Reset controller
-        VZController.Restart();
-
-        //// Reset state
-        //mSpeedMultiplier = 1.0f;
-
-        // Reset initial position and rotation
-        //transform.position = mInitialPos;
-        //GetComponent<Rigidbody>().velocity = Vector3.zero;
-        //mBodyRot = mInitialRot;
-
-        VZPlugin.ResetMotion();
-        IsCalibrated = true;
-        SetState(STATE_NORMAL);
-
-#if !VZ_GAME
-        StartCoroutine(FadeDown(2));
-#endif
-    }
 
     protected string State() {
         return _State;
@@ -206,20 +140,14 @@ public class VZMechController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-        if (!IsCalibrated) {
-            UpdateCalibrating();
-            return;
-        }
+    void Update() {
         HandleLeaning();
         HandleThrottle();
         HandleInput();
     }
 
     void FixedUpdate() {
-        if (!IsCalibrated) {
-            return;
-        }
+
         if (Motor.MechDisplays) {
             SpeedometerTicks++;
             if (SpeedometerTicks > TicksUntilSpeedometerRefresh) {
@@ -231,12 +159,13 @@ public class VZMechController : MonoBehaviour {
         InputSpeeds.Add(VZController.InputSpeed);
         if (InputSpeeds.Count >= InputSpeedsToAverage) {
             float sumSpeed = 0;
-            for (int i = 0; i< InputSpeeds.Count; i++) {
+            for (int i = 0; i < InputSpeeds.Count; i++) {
                 sumSpeed += InputSpeeds[i];
             }
-            AverageInputSpeed = (sumSpeed / InputSpeeds.Count) * 4;
-            //Debug.LogWarning("Avg Speed: " + AverageInputSpeed);
+            AverageInputSpeed = (sumSpeed / InputSpeeds.Count);
+            Debug.LogWarning("Avg Speed: " + AverageInputSpeed);
             InputSpeeds.Clear();
+            //Motor.HandleForwardMotion(AverageInputSpeed);
 
         }
     }
@@ -250,71 +179,21 @@ public class VZMechController : MonoBehaviour {
     }
     void HandleLeaning() {
         float leanPercent;
-        if (Mathf.Abs(VZController.HeadLean) > MinDistanceToLean) {
-            leanPercent = Mathf.Min((Mathf.Abs(VZController.HeadLean) - MinDistanceToLean), MaxLeanDelta) / MaxLeanDelta;
-            if (VZController.HeadLean <0) {
+        if (Mathf.Abs(HeadCamera.transform.localPosition.x) > MinDistanceToLean) {
+            leanPercent = Mathf.Min((Mathf.Abs(HeadCamera.transform.localPosition.x) - MinDistanceToLean), MaxLeanDelta) / MaxLeanDelta;
+            if (HeadCamera.transform.localPosition.x < 0) {
                 leanPercent *= -1;
             }
         }
         else {
             leanPercent = 0;
         }
+
         Motor.HandleStrafeMotion(leanPercent);
     }
 
     void HandleThrottle() {
-        if (BikeVelocity < AverageInputSpeed) {
-            BikeVelocity += Time.deltaTime * Acceleration;
-        }
-        else {
-            BikeVelocity -= Time.deltaTime * Acceleration;
-        }
-        BikeVelocity = Mathf.Clamp(BikeVelocity, 0, Motor.MaxForwardVelocity);
-        Motor.HandleForwardMotion(BikeVelocity);
+
+        Motor.HandleForwardMotion(AverageInputSpeed);
     }
 }
-//if (Mathf.Abs(ActualBikeZ) > 5) {
-//    float translateAmount = Time.deltaTime * (ActualBikeZ / LeanAngle) * BikeMotor.MaxStrafeVelocity * (BikeVelocity / BikeMotor.MaxForwardVelocity);
-//    BikeMotor.HandleStrafeMotion(translateAmount);
-//}
-//if (UsingHeadset) {
-//    headZAngle = CameraTransform.localRotation.eulerAngles.z;
-//    if (headZAngle > 180) {
-//        headZAngle -= 360;
-
-
-//        headZAngle = Mathf.Max(-40, headZAngle);
-//    }
-
-//    else {
-//        headZAngle = Mathf.Min(40, headZAngle);
-//    }
-//    ActualBikeZ = headZAngle;
-//}
-//else {
-//    if (StrafeInput != 0) {
-//        //Debug.Log(CameraRigPC.localEulerAngles.y);
-//        ActualBikeZ = Mathf.Lerp(ActualBikeZ, -StrafeInput * LeanAngle * StrafeThresholds.Evaluate(CameraRigPC.localEulerAngles.y), Time.deltaTime * 10);
-
-
-//        //ActualBikeZ += StrafeInput * Time.deltaTime * -LeanSpeed;
-//    }
-//    else {
-//        if (ActualBikeZ < 0) {
-//            ActualBikeZ += Time.deltaTime * LeanSpeed;
-//            if (ActualBikeZ > 0) {
-//                ActualBikeZ = 0;
-//            }
-//        }
-//        else if (ActualBikeZ > 0) {
-//            ActualBikeZ -= Time.deltaTime * LeanSpeed;
-//            if (ActualBikeZ < 0) {
-//                ActualBikeZ = 0;
-//            }
-//        }
-//    }
-
-//    //ActualBikeZ = Mathf.Lerp(ActualBikeZ, -StrafeInput * LeanAngle, Time.deltaTime);
-//    ActualBikeZ = Mathf.Clamp(ActualBikeZ, -LeanAngle, LeanAngle);
-
-//}
